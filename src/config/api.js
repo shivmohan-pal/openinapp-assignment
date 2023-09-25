bb03aede4e3d16e303588c67027531000b7f42fc
shivmohan-pal <shivmohan7895@gmail.com>
2023-09-25 15:21:23 +0530

    8

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const url = "http://34.198.81.140/attendance.json";
+export const url = "https://shivmohan-pal.github.io/data/attendance.json";
 
 export const Fetch = (url) =>{
  const [data , setData] = useState(null); 
