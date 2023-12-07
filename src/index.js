af740edc4ce02161b4bed0126252262762715269
shivmohan-pal <64728396+shivmohan-pal@users.noreply.github.com>
2023-12-07 14:27:33 +0530

    secured client_id

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-const client_Id = "276655891253-ekbvsrtif3v5jfitu7kvriui1p5rs3r9.apps.googleusercontent.com"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <GoogleOAuthProvider clientId={client_Id}>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
   </React.StrictMode>
